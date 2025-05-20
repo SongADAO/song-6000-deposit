ae5dea461ba6412dc9fad682382cd6f8752347dd
aLANparty <aLANparty@protonmail.com>
2025-05-20 12:06:11 -0400

    lock tests

--- test/Lock.ts
+++ test/Lock.ts
@@ -133,4 +133,122 @@ describe("Lock", function () {
       });
     });
   });
+
+  describe("Set Unlock Time", function () {
+    describe("Validations", function () {
+      it("Should revert if called before the unlock time", async function () {
+        const { lock } = await loadFixture(deployOneYearLockFixture);
+
+        // Try to set a new unlock time before the current unlock time has passed
+        await expect(lock.write.setUnlockTime([BigInt((await time.latest()) + 2 * 365 * 24 * 60 * 60)]))
+          .to.be.rejectedWith("You can't change time yet");
+      });
+
+      it("Should revert if called from another account", async function () {
+        const { lock, unlockTime, otherAccount } = await loadFixture(
+          deployOneYearLockFixture
+        );
+
+        // We can increase the time in Hardhat Network to pass the unlock time
+        await time.increaseTo(unlockTime);
+
+        // Try to set unlock time from another account
+        const lockAsOtherAccount = await hre.viem.getContractAt(
+          "Lock",
+          lock.address,
+          { client: { wallet: otherAccount } }
+        );
+        await expect(lockAsOtherAccount.write.setUnlockTime([unlockTime + 1n]))
+          .to.be.rejectedWith("You aren't the owner");
+      });
+
+      it("Should update the unlock time if called by the owner after unlock time", async function () {
+        const { lock, unlockTime } = await loadFixture(
+          deployOneYearLockFixture
+        );
+
+        // Increase time to pass the unlock time
+        await time.increaseTo(unlockTime);
+
+        // Set a new unlock time (one more year)
+        const newUnlockTime = unlockTime + BigInt(365 * 24 * 60 * 60);
+        await lock.write.setUnlockTime([newUnlockTime]);
+
+        // Check if the unlock time was updated
+        expect(await lock.read.unlockTime()).to.equal(newUnlockTime);
+      });
+    });
+  });
+
+  describe("Set Owner", function () {
+    describe("Validations", function () {
+      it("Should revert if called from another account", async function () {
+        const { lock, otherAccount } = await loadFixture(
+          deployOneYearLockFixture
+        );
+
+        // Try to set owner from another account
+        const lockAsOtherAccount = await hre.viem.getContractAt(
+          "Lock",
+          lock.address,
+          { client: { wallet: otherAccount } }
+        );
+        await expect(lockAsOtherAccount.write.setOwner([getAddress(otherAccount.account.address)]))
+          .to.be.rejectedWith("You aren't the owner");
+      });
+
+      it("Should update the owner if called by the current owner", async function () {
+        const { lock, owner, otherAccount } = await loadFixture(
+          deployOneYearLockFixture
+        );
+
+        // Set new owner
+        await lock.write.setOwner([getAddress(otherAccount.account.address)]);
+
+        // Check if the owner was updated
+        expect(await lock.read.owner()).to.equal(
+          getAddress(otherAccount.account.address)
+        );
+      });
+
+      it("Should allow the new owner to withdraw after ownership transfer", async function () {
+        const { lock, unlockTime, otherAccount, publicClient } = await loadFixture(
+          deployOneYearLockFixture
+        );
+
+        // Change owner to otherAccount
+        await lock.write.setOwner([getAddress(otherAccount.account.address)]);
+
+        // Increase time to pass the unlock time
+        await time.increaseTo(unlockTime);
+
+        // Try to withdraw with the new owner
+        const lockAsNewOwner = await hre.viem.getContractAt(
+          "Lock",
+          lock.address,
+          { client: { wallet: otherAccount } }
+        );
+
+        // Should succeed with the new owner
+        await expect(lockAsNewOwner.write.withdraw()).to.be.fulfilled;
+      });
+
+      it("Should prevent the old owner from withdrawing after ownership transfer", async function () {
+        const { lock, unlockTime, otherAccount } = await loadFixture(
+          deployOneYearLockFixture
+        );
+
+        // Change owner to otherAccount
+        await lock.write.setOwner([getAddress(otherAccount.account.address)]);
+
+        // Increase time to pass the unlock time
+        await time.increaseTo(unlockTime);
+
+        // Original owner should no longer be able to withdraw
+        await expect(lock.write.withdraw()).to.be.rejectedWith(
+          "You aren't the owner"
+        );
+      });
+    });
+  });
 });
