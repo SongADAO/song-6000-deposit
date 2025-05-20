228915e6071a8abd79cbda1c18f4f7b0a43a5c38
aLANparty <aLANparty@protonmail.com>
2025-05-20 12:31:28 -0400

    deploy setup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,65 @@
+import dotenv from "dotenv";
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
+dotenv.config();
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY || "",
+    currency: "USD",
+    enabled: process.env.REPORT_GAS !== undefined,
+    gasPrice: 3,
+  },
+  networks: {
+    // hardhat: {},
+    localhost: {
+      from: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", // want this contract owner
+      url: "http://127.0.0.1:8545",
+    },
+    mainnet: {
+      accounts: [process.env.MAINNET_PRIVATE_KEY ?? ''],
+      from: process.env.MAINNET_FROM, // want this contract owner
+      url: process.env.MAINNET_URL,
+    },
+    arbitrum: {
+      accounts: [process.env.MAINNET_PRIVATE_KEY ?? ''],
+      from: process.env.MAINNET_FROM, // want this contract owner
+      url: process.env.MAINNET_URL,
+    },
+    optimism: {
+      accounts: [process.env.MAINNET_PRIVATE_KEY ?? ''],
+      from: process.env.MAINNET_FROM, // want this contract owner
+      url: process.env.MAINNET_URL,
+    },
+    base: {
+      accounts: [process.env.MAINNET_PRIVATE_KEY ?? ''],
+      from: process.env.MAINNET_FROM, // want this contract owner
+      url: process.env.MAINNET_URL,
+    },
+    sepolia: {
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY ?? ''],
+      from: process.env.SEPOLIA_FROM, // want this contract owner
+      url: process.env.SEPOLIA_URL,
+    },
+    baseSepolia: {
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY ?? ''],
+      from: process.env.SEPOLIA_FROM, // want this contract owner
+      url: process.env.SEPOLIA_URL,
+    },
+  },
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
 };
 
 export default config;
--- ignition/modules/Lock.ts
+++ ignition/modules/Lock.ts
@@ -4,14 +4,18 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 import { parseEther } from "viem";
 
+const OWNER = '0xf1f6Ccaa7e8f2f78E26D25b44d80517951c20284';
 const JAN_1ST_2030 = 1893456000;
-const ONE_GWEI: bigint = parseEther("0.001");
+const ONE_HOUR_FROM_NOW = Math.floor(Date.now() / 1000) + 3600;
+// const ONE_GWEI: bigint = parseEther("0.001");
+const ZERO_GWEI: bigint = parseEther("0");
 
 const LockModule = buildModule("LockModule", (m) => {
-  const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
-  const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
+  const owner = m.getParameter("owner", OWNER);
+  const unlockTime = m.getParameter("unlockTime", ONE_HOUR_FROM_NOW);
+  const lockedAmount = m.getParameter("lockedAmount", ZERO_GWEI);
 
-  const lock = m.contract("Lock", [unlockTime], {
+  const lock = m.contract("Lock", [owner, unlockTime], {
     value: lockedAmount,
   });
 
