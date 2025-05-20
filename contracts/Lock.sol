4ef9ba075c1b4f0f4c1cabef4aba5bcb2e25c4fb
aLANparty <aLANparty@protonmail.com>
2025-05-20 11:58:04 -0400

    add mgmt features

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -10,14 +10,14 @@ contract Lock {
 
     event Withdrawal(uint amount, uint when);
 
-    constructor(uint _unlockTime) payable {
+    constructor(address _owner, uint _unlockTime) payable {
         require(
             block.timestamp < _unlockTime,
             "Unlock time should be in the future"
         );
 
         unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+        owner = payable(_owner);
     }
 
     function withdraw() public {
@@ -30,5 +30,18 @@ contract Lock {
         emit Withdrawal(address(this).balance, block.timestamp);
 
         owner.transfer(address(this).balance);
+    }
+
+    function setUnlockTime(uint _unlockTime) public {
+        require(block.timestamp >= unlockTime, "You can't change time yet");
+        require(msg.sender == owner, "You aren't the owner");
+
+        unlockTime = _unlockTime;
+    }
+
+    function setOwner(address _owner) public {
+        require(msg.sender == owner, "You aren't the owner");
+
+        owner = payable(_owner);
     }
 }
